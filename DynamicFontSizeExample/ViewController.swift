ea48418cb95f9f726397fc2a1bf5ec8d9ccba1ba
Jin Wang <jinw1990@gmail.com>
2016-03-22 19:48:16 +1100

    Initial Commit

---
+++ DynamicFontSizeExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DynamicFontSizeExample
+//
+//  Created by Jin Wang on 22/03/2016.
+//  Copyright © 2016 Uthoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
